7d581f1ee2a71eefb2751d1528e0e492764744f6
Leo <svntoleo@gmail.com>
2020-06-14 18:51:53 -0300

    Refactoring

--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,7 @@ server.use(cors());
 
 // Connect to database
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 const db = mongoose.connection;
 db.on('error', (error) => console.log(error));
 db.once('open', () => console.log('Connected to database!'));
